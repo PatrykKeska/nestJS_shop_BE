6a7045713d0a3406fe1285fff98cb6bf0c30afc8
PatrykKeska <PatrykKeska4@gmail.com>
2022-08-20 17:52:37 +0300

    * Basket Entity is ready to keep data about user and items. relations created.

--- src/entity/basket.entity.ts
+++ src/entity/basket.entity.ts
@@ -1,21 +1,27 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { userEntity } from './user.entity';
+import { shopItemsEntity } from './shopItems.entity';
 
 @Entity()
 export class basketEntity extends BaseEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column({
-    length: 60,
-  })
-  name: string;
-
-  @Column()
-  description: string;
-
-  @Column()
-  price: number;
-
   @Column()
   amount: number;
+
+  @ManyToOne(() => shopItemsEntity, (entity) => entity.itemsInBasket)
+  @JoinColumn()
+  items: shopItemsEntity;
+
+  @ManyToOne(() => userEntity, (entity) => entity.itemsInBasket)
+  @JoinColumn()
+  user: userEntity;
 }
